c4825596a1e195e73f7a737da515318220ff9594
Asam <asamvp@mail.ru>
2023-01-14 17:26:31 +0500

    доб задание 4

---
+++ 4/Program.cs
@@ -0,0 +1,7 @@
+﻿//функция
+Console.Write("Введите число, которое будете возводить в куб > ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int x = a * a * a;
+
+Console.WriteLine ($"Куб числа {a} равен {x}");
