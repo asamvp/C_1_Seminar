2d6588455938a425d4f667f16c28d148ada7e346
Asam <asamvp@mail.ru>
2023-01-14 17:20:48 +0500

    переделал задачу по неделе

--- 3/Program.cs
+++ 3/Program.cs
@@ -7,21 +7,11 @@ Console.Clear();
 System.Console.Write("Введите номер дня недели: ");
 String stringValue = Console.ReadLine();
 int value = Convert.ToInt32(stringValue);
-
-if(value==1){
-    System.Console.WriteLine($"День недели для номера {value} - понедельник");    
-}else if(value==2){
-    System.Console.WriteLine($"День недели для номера {value} - вторник");    
-}else if(value==3){
-    System.Console.WriteLine($"День недели для номера {value} - среда");    
-}else if(value==4){
-    System.Console.WriteLine($"День недели для номера {value} - четверг");    
-}else if(value==5){
-    System.Console.WriteLine($"УРА, День недели для номера {value} - пятница");    
-}else if(value==6){
-    System.Console.WriteLine($"День недели для номера {value} - суббота");    
-}else if(value==7){
-    System.Console.WriteLine($"День недели для номера {value} - воскресенье");    
-}else{
+if (value<8)
+{
+    string[] week = new string[] {"понедельник","вторник","среда","четверг","УРА, пятница","суббота","воскресенье"};
+    System.Console.WriteLine($"День недели для номера {value} - {week[value-1]}");   
+}
+else{
     System.Console.WriteLine($"Календарь сбился ))) нет такого дня: {value}");   
 }
